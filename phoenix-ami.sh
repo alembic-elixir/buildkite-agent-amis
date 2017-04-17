e7c892793209108181b2d70b0cbde5bb423886b4
James Sadler <freshtonic@gmail.com>
2017-04-17 13:57:14 +1000

    Ensure environment for buildkiet user has correct path
    Also refactored so package versions are defined in one place.

--- phoenix-ami.sh
+++ phoenix-ami.sh
@@ -35,17 +35,24 @@ sed -i "s/xxx/$BUILDKITE_TOKEN/g" /etc/buildkite-agent/buildkite-agent.cfg
 # Required by a specific Alembic client project
 pip install twisted
 
-# Run all commands from here under ec2-user account.
-time su - ec2-user <<EC2USER
+# Run all commands from here under buildkite-agent account.
+time su - buildkite-agent <<SCRIPT
 
 set -e -x -v
 
-git clone https://github.com/asdf-vm/asdf.git /home/ec2-user/.asdf --branch v0.3.0
+export BUILD_USER_HOME_DIR=/var/lib/buildkite-agent
 
-echo -e '\n. /home/ec2-user/.asdf/asdf.sh' >> ~/.bashrc
-echo -e '\n. /home/ec2-user/.asdf/completions/asdf.bash' >> ~/.bashrc
+export ERLANG_VERSION=19.3
+export ELIXIR_VERSION=1.4.2
+export NODEJS_VERSION=6.10.2
+export POSTGRES_VERSION=9.5.6
 
-export PATH=/home/ec2-user/.asdf/bin:$PATH
+git clone https://github.com/asdf-vm/asdf.git \$BUILD_USER_HOME_DIR/.asdf --branch v0.3.0
+
+echo -e "\n. \$BUILD_USER_HOME_DIR/.asdf/asdf.sh" >> ~/.bashrc
+echo -e "\n. \$BUILD_USER_HOME_DIR/.asdf/completions/asdf.bash" >> ~/.bashrc
+
+export PATH=\$BUILD_USER_HOME_DIR/.asdf/bin:\$PATH
 
 asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
 asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
@@ -54,21 +61,21 @@ asdf plugin-add postgres https://github.com/smashedtoatoms/asdf-postgres.git
 
 # Erlang compilation requires $HOME to be set. We aren't in a login
 # shell so we need to set it manually.
-export HOME=/home/ec2-user
+export HOME=\$BUILD_USER_HOME_DIR
 
-asdf install erlang 19.3
-asdf install elixir 1.4.2
+asdf install erlang \$ERLANG_VERSION
+asdf install elixir \$ELIXIR_VERSION
 # This is required for successful nodejs installation
-bash /home/ec2-user/.asdf/plugins/nodejs/bin/import-release-team-keyring
-asdf install nodejs 6.10.2
-asdf install postgres 9.5.6
+bash \$BUILD_USER_HOME_DIR/.asdf/plugins/nodejs/bin/import-release-team-keyring
+asdf install nodejs \$NODEJS_VERSION
+asdf install postgres \$POSTGRES_VERSION
 
-asdf local erlang 19.3
-asdf local elixir 1.4.2
-asdf local nodejs 6.10.2
-asdf local postgres 9.5.6
+asdf local erlang \$ERLANG_VERSION
+asdf local elixir \$ELIXIR_VERSION
+asdf local nodejs \$NODEJS_VERSION
+asdf local postgres \$POSTGRES_VERSION
 
-source /home/ec2-user/.asdf/asdf.sh
+source \$BUILD_USER_HOME_DIR/.asdf/asdf.sh
 
 # --force is supposed to not read from STDIN but it does, so we need
 # to pipe from "yes" to avoid termination of the heredoc.
@@ -81,9 +88,28 @@ yarn global add amphtml-validator
 yarn global add lighthouse
 yarn global add phantomjs-prebuilt
 
-echo -e "export PATH=$(yarn global bin):\$PATH" >> /home/ec2-user/.bashrc
+# We should be able to call 'yarn global bin' to get the path but for some
+# reason during user-data initialisation it is returning nothing.
+export YARN_GLOBAL_BIN_PATH=\$BUILD_USER_HOME_DIR/.asdf/installs/nodejs/\$NODEJS_VERSION/bin
+echo -e "export PATH=\$YARN_GLOBAL_BIN_PATH:\$PATH" >> \$BUILD_USER_HOME_DIR/.bashrc
 
-EC2USER
+ssh-keygen -t rsa -N "" -f \$BUILD_USER_HOME_DIR/.ssh/id_rsa
+
+pg_ctl start
+# createuser will fail if postgres has not started up
+sleep 5
+createuser postgres
+echo "ALTER USER postgres CREATEDB;" | psql -U buildkite-agent postgres
+
+SCRIPT
+
+sudo cp /etc/buildkite-agent/hooks/{pre-command.sample,pre-command}
+sudo chown buildkite-agent:buildkite-agent /etc/buildkite-agent/hooks/pre-command
+sudo chmod +x /etc/buildkite-agent/hooks/pre-command
+
+cat <<SCRIPT | sudo tee -a /etc/buildkite-agent/hooks/pre-command
+export PATH=/var/lib/buildkite-agent/.asdf/installs/nodejs/$NODEJS_VERSION/bin:/var/lib/buildkite-agent/.asdf/bin:/var/lib/buildkite-agent/.asdf/shims:/var/lib/buildkite-agent/.asdf/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/aws/bin:$PATH
+SCRIPT
 
 sudo service buildkite-agent start
 
